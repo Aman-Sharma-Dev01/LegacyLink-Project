5c2931eb1172b9024b2fc1ef4d29d1323584cffd
Aman Sharma <asharma8464@gmail.com>
2025-09-17 20:54:54 +0530

    .

--- alumni-portal-backend/server.js
+++ alumni-portal-backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 // ✅ Allow frontend access (React / Vite)
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://legacylink-project-glow.vercel.app'],
+    origin: ['http://localhost:3000', 'https://legacy-link-project.vercel.app'],
     credentials: true,
   })
 );
